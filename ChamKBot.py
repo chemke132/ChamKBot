f9ce61ba5fc05444f196d936e60a7a26bf39f592
DavinK <kimsasavin@gmail.com>
2025-08-26 23:58:57 -0700

    plans

--- ChamKBot.py
+++ ChamKBot.py
@@ -2,22 +2,77 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
 import random
+
+##유투브 음악 추출 관련
+from yt_dlp import YoutubeDL
+
+##디스코드 관련 
 import discord
 from discord.ext import commands
+from discord import app_commands
 
 intents = discord.Intents.default()
-intents.message_content = True
-intents.dm_messages = False
+intents.message_content = True ##채팅 가능
+intents.dm_messages = False    ##dm 불가능
 
+##명령어prefix -> !
 bot = commands.Bot(command_prefix=("!"), intents = intents) 
 
-@bot.command()
-async def 엄(ctx):
-    await ctx.send("준식")
+##테스트----------------------------------------------------
+@bot.event
+async def on_ready():
+    print("봇 실행")
 
+@bot.tree.command(name = "엄", description = "준식 임")
+async def 엄(interaction: discord.Interaction):
+    embed = discord.Embed(title = "test", color = 0x00ff00)
+    embed.add_field(name = "test2", value = "test3", inline = False)
+    await interaction.response.send_messsage(embed = embed)
+
+
+
+# @bot.command()##def [명령어](ctx) -> 명령어라는 채팅을 읽으면 동작
+# async def 엄(ctx):
+#     ##ctx.send -> 해당 채널에 채팅을 보냄
+    # await ctx.send("준식")
+
+
+##명령어----------------------------------------------------
+@bot.command()  
+async def 명령어(ctx):
+    await ctx.send("!룰렛: 룰렛 관련 명령어\n")
+
+
+##노래 관련------------------------------------------------
+# 
+# /재생 [음악이름 or url]
+# 
+# url 일 경우, 해당 영상을 바로 queue 에 추가 
+#  
+# 음악 이름일경ㅈ우, 음악 이름을 유투브에 검색 -> 상단 3개를 해당 채널에 팝업.
+# 3개중 선택하여 Queue 에 추가 
+# 
+# 이후 해당 영상에서 음악 추출
+# 
+# 추출 완료시 보이스챗에서 재생
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+
+
+##룰렛 관련---------------------------------------------------
 roulette = []
+
 
 @bot.command()
 async def 룰렛(ctx, action: str = '', *, content: str = ''):
